3fc98ce7fac4b15dad9cf78074a3e7eb5462fe7c
wbucko <bucko.wojciech@gmail.com>
2016-03-18 22:35:00 +0100

    Implemented Author create successful path

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -10,4 +10,21 @@ class AuthorsController < ApplicationController
 		@author = Author.new
 	end
 
+	def create
+		@author = Author.new(author_params)
+		if @author.save
+			flash[:success] = 'Author has been created'
+			redirect_to @author
+		else
+			flash[:danger] = ''
+			render :new
+		end
+	end
+
+
+	private
+	def author_params
+		params.require(:author).permit(:first_name, :last_name)
+	end
+
 end
--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -26,4 +26,30 @@ RSpec.describe AuthorsController, type: :controller do
 		end
 	end
 
+	describe 'POST #create' do 
+		context 'a successful create' do 
+			it 'saves the new author object' do 
+				post :create, author: Fabricate.attributes_for(:author)
+
+				expect(Author.count).to eq(1)
+			end
+
+			it 'redirects to the author show action' do 
+				post :create, author: Fabricate.attributes_for(:author)
+
+				expect(response).to redirect_to author_path(Author.last)
+			end
+
+			it 'sets the success flash message' do 
+				post :create, author: Fabricate.attributes_for(:author)
+
+				expect(flash[:success]).to eq('Author has been created')
+			end
+		end
+
+		context 'an unsuccessful create' do 
+
+		end
+	end
+
 end
